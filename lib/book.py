c18e54db1690d0c05e14716b7a06e8fe9a1bd81d
Vivian Gichure <viviangichure@gmail.com>
2025-08-09 15:56:31 +0300

    completed lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,34 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    # title property
+    def get_title(self):
+        return self._title
+
+    def set_title(self, value):
+        if isinstance(value, str):
+            self._title = value
+        else:
+            print("Title must be a string.")
+
+    title = property(get_title, set_title)
+
+    # page_count property
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    page_count = property(get_page_count, set_page_count)
+
+    # extra method (optional for reading)
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,54 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self._color = None
+        self._material = None
+        self.condition = None  # Initialize condition attribute
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        self._brand = value
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, value):
+        if isinstance(value, str) and value.strip():
+            self._color = value
+        else:
+            raise ValueError("Color must be a non-empty string.")
+
+    @property
+    def material(self):
+        return self._material
+
+    @material.setter
+    def material(self, value):
+        if isinstance(value, str):
+            self._material = value
+        else:
+            print("Material must be a string.")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"  # Set condition after repair
